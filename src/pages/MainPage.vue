6a8d1337ff1ac033e2b10623039b836cfcad63d0
Danil <sakralbar0192@gmail.com>
2023-06-19 21:19:41 +0800

    add links

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,11 @@
 <template>
     <div :class="$style.appWrapper">
+        <div>
+            <a href="https://sakralbar0192.github.io/MitraSoftTest/#/">Other apps</a>
+        </div>
+        <div>
+            <a href="https://github.com/sakralbar0192/CyberZilla">Source code</a>
+        </div>
         <h1>{{ store.title }}</h1>
         <router-view />
     </div>
