4ad99540a080ec0c0baf80ecce6aa443aa874151
Danil <sakralbar0192@gmail.com>
2023-06-19 21:31:07 +0800

    fix 404page

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,10 +1,32 @@
 <template>
-    NotFound
+    <div :class="$style.wrapper">
+        <v-btn
+            variant="outlined"
+            @click="router.back()"
+        >
+            Back to user list
+        </v-btn>
+        <p :class="$style.text">
+            There is no such page
+        </p>
+    </div>
+
 </template>
 
 <script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter()
 </script>
 
 <style module lang="scss">
+    .wrapper {
+        padding: 10px;
+    }
+    .text {
+        margin: 10px;
+        text-align: center;
+        font-weight: bold;
+    }
 </style>
 
